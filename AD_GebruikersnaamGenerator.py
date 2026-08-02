75e235af9f53761e448c1a46b50e79bc62cb80cb
Mirkan-23 <mirkan070@outlook.com>
2026-08-02 17:56:03 +0200

    V2

--- AD_GebruikersnaamGenerator.py
+++ AD_GebruikersnaamGenerator.py
@@ -80,27 +80,86 @@ afdelingen = {
     ]
 }
 
+AfdelingsVerdeling = {
+    "IT": 18,
+    "HR": 37,
+    "Financieel": 31,
+    "Sales": 75,
+    "Marketing": 85,
+    "Management": 24,
+    "Operaties": 170,
+    "Ondersteuning": 65,
+    "Juridisch": 20,
+    "Logistiek": 45,
+    "Administratie": 70,
+    "Onderzoek en Ontwikkeling": 40
+}
+
+
 # Genereer nummer beginnend met +31 6 en forceer altijd een nummer van 8 karakters na +31 6
 def werktelnummer():
-    return("+31 6" + str(fake.random_number(digits=8, fix_len=True)))
+    return("+31-6" + str(fake.random_number(digits=8, fix_len=True)))
 
-for nummer in range(100):
+aantalIT = 0
+aantalHR = 0
+aantalFinan = 0
+aantalSales = 0
+aantalMarketing = 0
+aantalManagement = 0
+aantalOperaties = 0
+aantalOndersteuning = 0
+aantalJuridisch = 0
+aantalLogistiek = 0
+aantalAdministratie = 0
+aantalOnderzoekOntwikkel = 0
 
-    user_id = nummer + 1
-    voornaam = fake.first_name()
-    achternaam = fake.last_name()
+user_id = 1
 
-    naam = voornaam + " " + achternaam
-    email = naam[:3].lower()+achternaam.replace(" ", "").lower()+"@bedrijf.local"
-    gebruikersnaam = voornaam[:3].lower() + achternaam.replace(" ", "").lower()
+for afdeling, aantal in AfdelingsVerdeling.items():
+    for _ in range(aantal):
+        user_id += 1
+        voornaam = fake.first_name()
+        achternaam = fake.last_name()
 
-    telnr = fake.phone_number()
+        naam = voornaam + " " + achternaam
+        email = naam[:3].lower()+achternaam.replace(" ", "").lower()+"@bedrijf.local"
+        gebruikersnaam = voornaam[:3].lower() + achternaam.replace(" ", "").lower()
 
-    if nummer < 15:
-        afdeling = "IT"
-    else:
-        afdeling = random.choice(list(afdelingen.keys()))
+        telnr = fake.phone_number()
 
-    functie = random.choice(afdelingen[afdeling])
+        # if nummer < 15:
+        #     afdeling = "IT"
+        # else:
+        #     afdeling = random.choice(list(afdelingen.keys()))
 
-    print(f"{user_id},{naam},{email},{werktelnummer()},{gebruikersnaam},{afdeling},{functie}")
+        if afdeling == "IT":
+            aantalIT += 1
+        if afdeling == "HR":
+            aantalHR += 1
+        if afdeling == "Financieel":
+            aantalFinan += 1
+        if afdeling == "Sales":
+            aantalSales += 1
+        if afdeling == "Marketing":
+            aantalMarketing += 1
+        if afdeling == "Management":
+            aantalManagement += 1
+        if afdeling == "Operaties":
+            aantalOperaties += 1
+        if afdeling == "Ondersteuning":
+            aantalOndersteuning += 1
+        if afdeling == "Juridisch":
+            aantalJuridisch += 1
+        if afdeling == "Logistiek":
+            aantalLogistiek += 1
+        if afdeling == "Administratie":
+            aantalAdministratie += 1
+        if afdeling == "Onderzoek en Ontwikkeling":
+            aantalOnderzoekOntwikkel += 1
+
+        functie = random.choice(afdelingen[afdeling])
+
+        print(f"{user_id},{naam},{email},{werktelnummer()},{gebruikersnaam},{afdeling},{functie}")
+
+print(f"\nAantal personeel per afdeling:\n-------------------------\nIT: {aantalIT}\nHR: {aantalHR}\nFinanciëel: {aantalFinan}\nSales: {aantalSales}\nMarketing: {aantalMarketing}\nOperaties: {aantalOperaties}\nOndersteuning {aantalOndersteuning}\nJuridisch: {aantalJuridisch}\nLogistiek: {aantalLogistiek}\nAdministratie: {aantalAdministratie}\nOnderzoek en Ontwikkeling: {aantalOnderzoekOntwikkel}\n-------------------------")
+print(f"Totaal aantal personeel: {sum(AfdelingsVerdeling.values())}\n-------------------------")
