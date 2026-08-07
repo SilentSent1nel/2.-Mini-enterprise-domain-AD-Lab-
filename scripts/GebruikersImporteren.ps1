fbbc94b7db897201ae740746ca68b778441492fc
Mirkan-23 <mirkan070@outlook.com>
2026-08-08 00:51:18 +0200

    Script bijgewerkt

---
+++ scripts/GebruikersImporteren.ps1
@@ -0,0 +1,34 @@
+$gebruikers = Import-Csv "C:\AD-Import\gebruikers.csv" 
+
+foreach ($gebruiker in $gebruikers) {
+    Write-Host "Naam:" $gebruiker.cn
+    Write-Host "Voornaam:" $gebruiker.givenName
+    Write-Host "Achternaam:" $gebruiker.sn
+    Write-Host "Username:" $gebruiker.sAMAccountName
+    Write-Host "Afdeling:" $gebruiker.department
+    Write-Host "Functie:" $gebruiker.title
+    Write-Host "-------------------------"
+
+    New-ADUser `
+    -Name $gebruiker.cn `
+    -GivenName $gebruiker.givenName `
+    -Surname $gebruiker.sn `
+    -DisplayName $gebruiker.displayname `
+    -SamAccountName $gebruiker.sAMAccountName `
+    -UserPrincipalName $gebruiker.userPrincipalName `
+    -EmployeeID $gebruiker.employeeID `
+    -Department $gebruiker.department `
+    -Title $gebruiker.title `
+    -Path "OU=$($gebruiker.department),OU=Gebruikers,DC=bedrijf,DC=local" `
+    -Enabled $true `
+    -AccountPassword (ConvertTo-SecureString "Welkom123!" -AsPlainText -Force)
+
+
+    $groepen = $gebruiker.Groups -split ";"
+
+    foreach ($groep in $groepen) {
+        Add-ADGroupMember `
+        -Identity $groep `
+        -Members $gebruiker.sAMAccountName
+    }
+}
